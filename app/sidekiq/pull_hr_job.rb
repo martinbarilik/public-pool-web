60c1b21add1d98d45a4188e5051888711e8a8ff9
Martin Barilik <barilik@proton.me>
2026-06-13 17:13:53 +0200

    refactor: add conditional update with compact_blank for user data processing

--- app/sidekiq/pull_hr_job.rb
+++ app/sidekiq/pull_hr_job.rb
@@ -60,7 +60,10 @@ class PullHrJob
 
 	def process_user_data(user, json, current_time)
 		workers, best_difficulty, workers_count = json.values_at(:workers, :bestDifficulty, :workersCount)
-		user.update!(best_difficulty:, workers_count:)
+
+		updates = { best_difficulty: best_difficulty, workers_count: workers_count }.compact_blank
+
+		user.update!(updates) if updates.any?
 
 		workers.each do |worker_hash|
 			worker = user.workers.find_by(name: worker_hash[:name])
