79522535c766bf0f043d43ffd2593d4ba83291f2
Martin Barilik <barilik@proton.me>
2026-06-13 17:16:53 +0200

    refactor: use bulk change_table for removing pool column defaults

--- db/migrate/20260612000001_remove_platform_defaults_from_pools.rb
+++ db/migrate/20260612000001_remove_platform_defaults_from_pools.rb
@@ -2,7 +2,9 @@
 
 class RemovePlatformDefaultsFromPools < ActiveRecord::Migration[8.0]
 	def change
-		change_column_default :pools, :host, from: '127.0.0.1', to: nil
-		change_column_default :pools, :port, from: '2019', to: nil
+		change_table :pools, bulk: true do |t|
+			t.change_default :host, from: '127.0.0.1', to: nil
+			t.change_default :port, from: '2019', to: nil
+		end
 	end
 end
