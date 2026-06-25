2b74bf79e9e118f16f80e15efe1dc1a4e2511d65
Martin Barilik <barilik@proton.me>
2026-06-25 21:43:45 +0200

    refactor: add Turbo Stream broadcasting to Pool model for real-time updates

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Pool < ApplicationRecord
+	include ActionView::RecordIdentifier
+
 	DEFAULT_HOST = ENV.fetch('PUBLIC_POOL_HOST', '127.0.0.1')
 	DEFAULT_PORT = ENV.fetch('PUBLIC_POOL_PORT', '2019')
 
 	validates :host, :port, presence: true
+
+	after_save_commit :broadcast_update
 
 	def self.main
 		first_or_create(host: DEFAULT_HOST, port: DEFAULT_PORT)
@@ -13,5 +17,18 @@ class Pool < ApplicationRecord
 	def eval_period
 		num, unit = period.split('.')
 		num.to_i.send(unit).ago
+	end
+
+	private
+
+	def broadcast_update
+		broadcast_replace_to(
+			dom_id(self),
+			target: dom_id(self),
+			partial: 'pools/info',
+			locals: {
+				pool: self
+			}
+		)
 	end
 end
